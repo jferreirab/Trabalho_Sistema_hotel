2faf8451b8892db5a09567d748a8f945b2cd9ccf
jferreirab <jferreirab@yahoo.com.br>
2016-11-22 20:32:52 -0200

    alteração situacao reserva

---
+++ AplicacaoHotel/src/br/com/hotel/model/service/ISituacaoReservaService.java
@@ -0,0 +1,8 @@
+package br.com.hotel.model.service;
+
+import br.com.hotel.model.domain.SituacaoReserva;
+
+public interface ISituacaoReservaService {
+
+	public  SituacaoReserva[] buscarTodos();
+}
